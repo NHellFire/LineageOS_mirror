a9fab7163162fcb61d1bdd23a0fc2294469ece14
NHellFire <nathan.renniewaldock@gmail.com>
2013-11-28 00:23:58 +0000

    Scripts/manifest-all/aosp.sh: Sort before saving

--- Scripts/manifest-all/aosp.sh
+++ Scripts/manifest-all/aosp.sh
@@ -3,5 +3,4 @@ URLS=""
 for branch in gingerbread ics jellybean cm-10.1 cm-10.2 cm-11.0; do
 	URLS+="https://raw.github.com/CyanogenMod/android/$BRANCH/default.xml "
 done
-curl --compressed $URLS | grep 'remote="aosp"' | grep -Eo 'name=".*"' | cut -d'"' -f2 > aosp.txt
-sort -u -o aosp.txt aosp.txt
+curl --compressed $URLS | grep 'remote="aosp"' | grep -Eo 'name=".*"' | cut -d'"' -f2 | sort -u > aosp.txt
