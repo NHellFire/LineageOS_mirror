d88674a827dd8394f1471a28423cf518ca01ac35
NHellFire <nathan.renniewaldock@gmail.com>
2013-11-28 00:42:20 +0000

    Split device trees out of main manifests too

--- Scripts/split-kernels_and_devices-manifest/split.sh
+++ Scripts/split-kernels_and_devices-manifest/split.sh
@@ -13,5 +13,14 @@ for manifest in *.xml; do
 		grep -Ev "CyanogenMod/(android_|\w+-)kernel" "$manifest" > "$manifest.tmp"
 		mv "$manifest.tmp" "$manifest"
 	fi
-	rm "$KERNEL_MANIFEST.tmp"
+
+	DEVICE_MANIFEST="${manifest/.xml/_devices.xml}"
+	grep -E "CyanogenMod/android_device_" "$manifest" > "$DEVICE_MANIFEST.tmp"
+	if [ -s "$DEVICE_MANIFEST.tmp" ]; then
+		cat "manifest.xml.head" "$DEVICE_MANIFEST.tmp" "manifest.xml.tail" > "$DEVICE_MANIFEST"
+		grep -Ev "CyanogenMod/android_device_" "$manifest" > "$manifest.tmp"
+		mv "$manifest.tmp" "$manifest"
+	fi
+
+	rm "$KERNEL_MANIFEST.tmp" "$DEVICE_MANIFEST.tmp"
 done
