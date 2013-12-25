237bf3f5768955efb28b66427a4dcbbd8bf0559c
NHellFire <nathan.renniewaldock@gmail.com>
2013-12-25 20:03:20 +0000

    Scripts/manifest-all/aosp.sh: Remove bash features and fix typos

--- Scripts/manifest-all/aosp.sh
+++ Scripts/manifest-all/aosp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 URLS=""
-for branch in gingerbread ics jellybean cm-10.1 cm-10.2 cm-11.0; do
-	URLS+="https://raw.github.com/CyanogenMod/android/$BRANCH/default.xml "
+for BRANCH in gingerbread ics jellybean cm-10.1 cm-10.2 cm-11.0; do
+	URLS="$URLS https://raw.github.com/CyanogenMod/android/$BRANCH/default.xml"
 done
 curl --compressed $URLS | grep 'remote="aosp"' | grep -Eo 'name=".*"' | cut -d'"' -f2 | sort -u > aosp.txt
