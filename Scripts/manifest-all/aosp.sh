c98c7762b3d4cae2ff3e3cf1554412470e909d4d
NHellFire <nathan.renniewaldock@gmail.com>
2014-05-15 00:39:22 +0100

    Scripts/manifest-all/aosp.sh: Make curl follow redirects

--- Scripts/manifest-all/aosp.sh
+++ Scripts/manifest-all/aosp.sh
@@ -3,4 +3,4 @@ URLS=""
 for BRANCH in gingerbread ics jellybean cm-10.1 cm-10.2 cm-11.0; do
 	URLS="$URLS https://raw.github.com/CyanogenMod/android/$BRANCH/default.xml"
 done
-curl --compressed $URLS | grep 'remote="aosp"' | grep -Eo 'name=".*"' | cut -d'"' -f2 | sort -u > aosp.txt
+curl --compressed -L $URLS | grep 'remote="aosp"' | grep -Eo 'name=".*"' | cut -d'"' -f2 | sort -u > aosp.txt
