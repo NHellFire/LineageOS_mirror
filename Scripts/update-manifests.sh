f75a4b958a2a6c505084942662707c14310c227c
NHellFire <nathan.renniewaldock@gmail.com>
2014-06-26 17:50:49 +0100

    Scripts/update-manifests.sh: Add script to update all manifests

---
+++ Scripts/update-manifests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+BRANCHES="cm-10.1 cm-10.2 cm-11.0"
+
+(cd manifest-all && ./aosp.sh && ./mkmanifest.py)
+mv manifest-all/default.xml split-kernels_and_devices-manifest/
+
+for branch in $BRANCHES; do
+	echo $branch
+	(cd manifest-single-branch && ./mkmanifest.sh $branch)
+	mv manifest-single-branch/$branch.xml split-kernels_and_devices-manifest/
+done
+
+(cd split-kernels_and_devices-manifest && ./split.sh)
+mv split-kernels_and_devices-manifest/*.xml ..
