dd525d693e6ddb0024d09e16d8a2fa9a80895c80
NHellFire <nathan.renniewaldock@gmail.com>
2015-09-19 23:25:01 +0100

    Add cm-12.1

--- Scripts/update-manifests.sh
+++ Scripts/update-manifests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-BRANCHES="cm-10.1 cm-10.2 cm-11.0 cm-12.0"
+BRANCHES="cm-10.1 cm-10.2 cm-11.0 cm-12.0 cm-12.1"
 
 (cd manifest-all && ./aosp.sh && ./mkmanifest.py)
 mv manifest-all/default.xml split-kernels_and_devices-manifest/
