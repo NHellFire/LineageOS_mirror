cbad679d719bdd295a65b7dc2fab44815d9e87d3
NHellFire <nathan.renniewaldock@gmail.com>
2016-03-18 14:55:00 +0000

    Add cm-13.0

--- Scripts/manifest-all/aosp.sh
+++ Scripts/manifest-all/aosp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 URLS=""
-for BRANCH in cm-10.1 cm-10.2 cm-11.0 cm-12.0 cm-12.1; do
+for BRANCH in cm-10.1 cm-10.2 cm-11.0 cm-12.0 cm-12.1 cm-13.0; do
 	URLS="$URLS https://raw.github.com/CyanogenMod/android/$BRANCH/default.xml"
 done
 curl --compressed -L $URLS | grep 'remote="aosp"' | grep -Eo 'name=".*"' | cut -d'"' -f2 | sort -u > aosp.txt
