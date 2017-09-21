06f465daa2a6e8c94712b4e15e7577eefe4283c8
NHellFire <nathan.renniewaldock@gmail.com>
2017-09-21 15:45:39 +0100

    Add lineage-15.0

--- Scripts/manifest-all/aosp.sh
+++ Scripts/manifest-all/aosp.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 URLS=""
-for BRANCH in cm-10.1 cm-10.2 cm-11.0 cm-12.0 cm-12.1 cm-13.0; do
+BRANCHES="${BRANCHES:-cm-10.1 cm-10.2 cm-11.0 cm-12.0 cm-12.1 cm-13.0 lineage-15.0}"
+
+for BRANCH in $BRANCHES; do
 	URLS="$URLS https://raw.github.com/LineageOS/android/$BRANCH/default.xml"
 done
 curl --compressed -L $URLS | grep 'remote="aosp"' | grep -Eo 'name=".*"' | cut -d'"' -f2 | sort -u > aosp.txt
--- Scripts/update-manifests.sh
+++ Scripts/update-manifests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-BRANCHES="cm-10.1 cm-10.2 cm-11.0 cm-12.0 cm-12.1 cm-13.0 cm-14.1"
+export BRANCHES="cm-10.1 cm-10.2 cm-11.0 cm-12.0 cm-12.1 cm-13.0 cm-14.1 lineage-15.0"
 
 (cd manifest-all && ./aosp.sh && ./mkmanifest.py)
 mv manifest-all/default.xml split-kernels_and_devices-manifest/
